67a7f77b6c628eacff26d9b7fa1074a1a6134655
Marcos Rigel <marcosrigel1999@gmail.com>
2025-06-15 23:20:38 -0300

    atualizando o excluir entrega

--- excluir_entrega.php
+++ excluir_entrega.php
@@ -2,13 +2,13 @@
 session_start();
 include("config.php");
 
-if (!isset($_GET['id']) || !isset($_SESSION['id_fornecedor'])) {
+if (!isset($_GET['id']) || !isset($_SESSION['id_usuario'])) {
     header("Location: visualizar.php");
     exit;
 }
 
 $id = intval($_GET['id']);
-$id_fornecedor = $_SESSION['id_fornecedor'];
+$id_fornecedor = $_SESSION['id_usuario'];
 
 $stmt = $conn->prepare("DELETE FROM entregas WHERE id = ? AND id_fornecedor = ?");
 $stmt->bind_param("ii", $id, $id_fornecedor);
